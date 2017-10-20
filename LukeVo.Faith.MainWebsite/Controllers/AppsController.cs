416b860fa4d73c6debca928032e092a54309d064
Luke Vo <datvm@outlook.com>
2017-10-21 01:09:16 +0700

    Added a new Main Website running on .NET Core 2.

---
+++ LukeVo.Faith.MainWebsite/Controllers/AppsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LukeVo.Faith.MainWebsite.Controllers
+{
+    public class AppsController : Controller
+    {
+
+        [Route("daily-bible-verse")]
+        public IActionResult DailyBibleVerse()
+        {
+            return this.View();
+        }
+
+    }
+}
